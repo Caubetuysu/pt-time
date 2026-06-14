13edd4f3464bd6bd56546d8f5faa81782d518100
Tran Phuoc Thanh <tranphuocthanh2k8@gmail.com>
2026-06-15 00:26:58 +0700

    Implement Fullscreen Focus Mode and fix timer overflow

--- frontend/app.js
+++ frontend/app.js
@@ -235,6 +235,33 @@ window.alert = function(message) {
 // --- Event Listeners ---
 function initEventListeners() {
 
+    // Fullscreen Feature
+    const fullscreenBtn = document.getElementById('fullscreen-btn');
+    if (fullscreenBtn) {
+        fullscreenBtn.addEventListener('click', () => {
+            if (!document.fullscreenElement) {
+                document.documentElement.requestFullscreen().catch(err => {
+                    console.log(`Error attempting to enable full-screen mode: ${err.message}`);
+                });
+            } else {
+                if (document.exitFullscreen) {
+                    document.exitFullscreen();
+                }
+            }
+        });
+    }
+
+    document.addEventListener('fullscreenchange', () => {
+        if (document.fullscreenElement) {
+            document.body.classList.add('is-fullscreen-timer');
+            fullscreenBtn.querySelector('svg').innerHTML = '<path d="M8 3v3a2 2 0 0 1-2 2H3m18 0h-3a2 2 0 0 1-2-2V3m0 18v-3a2 2 0 0 1 2-2h3M3 16h3a2 2 0 0 1 2 2v3"/>';
+        } else {
+            document.body.classList.remove('is-fullscreen-timer');
+            fullscreenBtn.querySelector('svg').innerHTML = '<path d="M8 3H5a2 2 0 0 0-2 2v3m18 0V5a2 2 0 0 0-2-2h-3m0 18h3a2 2 0 0 0 2-2v-3M3 16v3a2 2 0 0 0 2 2h3"/>';
+        }
+    });
+
+
     // Subject selector
     subjectSelect.addEventListener('change', (e) => {
         if (e.target.value === 'add-new') {
